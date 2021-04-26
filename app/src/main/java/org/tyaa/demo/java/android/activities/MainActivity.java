8137291006dd5f80a765d955784a883ab6d65668
YuriiTrofimenko <tyaa@ukr.net>
2021-04-26 20:09:52 +0300

    init, activities, views, on click event handler, toast

---
+++ app/src/main/java/org/tyaa/demo/java/android/activities/MainActivity.java
@@ -0,0 +1,34 @@
+package org.tyaa.demo.java.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button actionButton = findViewById(R.id.mainActionButton);
+        /* actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Hello Java Android Toast!",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        }); */
+        actionButton.setOnClickListener(
+                v -> Toast.makeText(
+                    MainActivity.this,
+                    "Hello Java Android Toast!",
+                    Toast.LENGTH_LONG
+                ).show()
+        );
+    }
+}
