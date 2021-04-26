5ba67b3e48d2977ec029f994d60b38d9ffe14470
YuriiTrofimenko <tyaa@ukr.net>
2021-04-26 21:05:16 +0300

    explicit intent, setActivityForResult, onActivityResult

--- app/src/main/java/org/tyaa/demo/java/android/activities/FormActivity.java
+++ app/src/main/java/org/tyaa/demo/java/android/activities/FormActivity.java
@@ -1,7 +1,10 @@
 package org.tyaa.demo.java.android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        if (getIntent().hasExtra("message")) {
+            Toast.makeText(
+                    FormActivity.this,
+                    getIntent().getStringExtra("message"),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+        ((EditText)findViewById(R.id.formMessageEditText)).setOnKeyListener((v, keyCode, event) -> {
+            Intent intent = new Intent();
+            intent.putExtra("feedback", ((EditText)findViewById(R.id.formMessageEditText)).getText().toString());
+            setResult(RESULT_OK, intent);
+            return false;
+        });
     }
+
+    /* @Override
+    protected void onDestroy() {
+        System.out.println("MyDebug: FormActivity onDestroy Start");
+        System.out.println(((EditText)findViewById(R.id.formMessageEditText)).getText());
+        System.out.println("MyDebug: FormActivity onDestroy Finish");
+        super.onDestroy();
+    } */
 }
--- app/src/main/java/org/tyaa/demo/java/android/activities/MainActivity.java
+++ app/src/main/java/org/tyaa/demo/java/android/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package org.tyaa.demo.java.android.activities;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,12 +26,37 @@ public class MainActivity extends AppCompatActivity {
                 ).show();
             }
         }); */
-        actionButton.setOnClickListener(
+        /* actionButton.setOnClickListener(
                 v -> Toast.makeText(
                     MainActivity.this,
                     "Hello Java Android Toast!",
                     Toast.LENGTH_LONG
                 ).show()
+        ); */
+        actionButton.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                    EditText messageEditText = findViewById(R.id.mainMessageEditText);
+                    intent.putExtra("message", messageEditText.getText().toString());
+                    // startActivity(intent);
+                    startActivityForResult(intent, 0);
+                }
         );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("MyDebug: MainActivity Start");
+        System.out.println("data = " + data);
+        if (data != null && data.hasExtra("feedback")) {
+            System.out.println("data.hasExtra(\"feedback\") = " + data.hasExtra("feedback"));
+            Toast.makeText(
+                    MainActivity.this,
+                    data.getStringExtra("feedback"),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+        System.out.println("MyDebug: MainActivity End");
     }
 }
